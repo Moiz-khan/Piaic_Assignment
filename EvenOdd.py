54a67f961add79baf17f5bef4aee926439273cb4
Moiz khan <smartmoeez@gamil.com>
2019-05-09 23:28:12 +0500

    First commit

---
+++ EvenOdd.py
@@ -0,0 +1,20 @@
+#Even odd user defined function
+
+print("Enter the number : ")
+Num = 5
+Even_Odd(Num)
+    
+
+def Even_Odd(num):
+ {
+    if(num%2==0):
+     {
+        print("Even!")
+     }
+    else:
+     {
+         print("Odd!")
+     }
+ }
+
+
